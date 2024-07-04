56ed39f5a5b55237a8075472dd409a879b949e79
Nishit Patel <nishitpatel.dev@gmail.com>
2024-07-04 21:30:59 +0530

    Implement ProtectRoute for handling user authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,40 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const About = React.lazy(() => import("./pages/About"));
 const Login = React.lazy(() => import("./pages/Login"));
 const Chat = React.lazy(() => import("./pages/Chat"));
 const Groups = React.lazy(() => import("./pages/Groups"));
+const Error = React.lazy(() => import("./pages/Error"));
 
+let user = false;
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
-        <Route path="/login" element={<Login />} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+          </Route>
 
-      </Routes>
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
